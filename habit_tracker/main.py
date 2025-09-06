0d0da6f150f1a31bc8330a85fa01cbe218c28688
Alexandranuthu <alexandranuthuu@gmail.com>
2025-09-06 19:15:53 +0300

    Add ask the user for input

---
+++ habit_tracker/main.py
@@ -0,0 +1,10 @@
+habits = []
+
+no_habits = int(input("How many habits do you want to enter today? "))
+
+# for loop to take each input one by one and append to the list 
+# using range
+for h in range(no_habits):
+    # after entering habit, allow user to add another one
+    user_habit = input(f"Enter habit {h + 1}")
+    habits.append(user_habit)
