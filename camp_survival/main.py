9611f8275852d18ff9d35133b4f0a0e82cf39549
Alexandranuthu <alexandranuthuu@gmail.com>
2025-09-07 20:32:09 +0300

    Update formatted the output

--- camp_survival/main.py
+++ camp_survival/main.py
@@ -1,6 +1,6 @@
 import random
 
-print("\n--------------- Welcome to Camp Survival ---------------")
+print("\n 🛶🌲--------------- Welcome to Camp Survival --------------- 🛶🌲")
 
 # dic to store the initial resources
 camp = {
@@ -16,11 +16,11 @@ won = False
 
 def showStatus():
     print(f"\nYou are on day {camp['day']}")
-    print(f"So far you are left with \n\t Food: {camp['food']} \n\t Health: {camp['health']} \n\t Survivors: {camp['survivors']}")
+    print(f"So far you are left with \n\t 🥫Food: {camp['food']} \n\t ❤️Health: {camp['health']} \n\t 🧍Survivors: {camp['survivors']}")
 
 
 def gatherfood():
-    print("You have decided to gather food. You need to choose the number of survivors you want to go gather food")
+    print("\n🚶You decided to gather food. You need to choose the number of survivors you want to go gather food")
     no_survivors = int(input("How many? "))
     # randomly choosing the number of food brought back, the more people sent the more food returned
     food_found = sum(random.randint(0,5) for _ in range(no_survivors))
@@ -28,17 +28,17 @@ def gatherfood():
     # each time they gather food they lose health
     camp['health'] = max(0, camp['health'] - 10)
 
-    print(f"The {no_survivors} survivors brought back {food_found} food.\nYou now have {camp['food']}")
-    print(f"Your health got depleted though. Rem: {camp['health']} ")
+    print(f"🌽 The {no_survivors} survivors brought back {food_found} units of food.\n\tTotal food now: {camp['food']}")
+    print(f"\t💔Camp health depleted though: {camp['health']} ")
 
 def rest():
-    print("You have decided to rest.")
+    print("\n🛌You chose to rest.")
     # this adds 5 to the health key value once they've rested and making sure health doesn't go past 100
     camp['health'] = min(100, camp['health'] + 5)
-    print(f"Camp's health is now at {camp['health']}")
+    print(f"\t💖They needed that rest. Restored to: {camp['health']}")
 
 def feedsurvivors():
-    print(f"You need to feed the people.You have {camp['food']} in store")
+    print(f"\n🍽️You need to feed your people.\n\t🥫Food available: {camp['food']}")
     # check if food is more than the survivors,if not else block runs
     if camp['food'] >= camp['survivors']:
         # each person can get 1 food remainder is assigned back to the food store
@@ -46,45 +46,49 @@ def feedsurvivors():
         camp['food'] = divide_food
         # this adds 15 to the health key value once they've eaten
         camp['health'] = min(100, camp['health'] + 15)
-        print(f"Camp has eaten. Health has been added: {camp['health']}.")
+        print(f"✅Camp has eaten. They feel better now.\n\t❤️Health:{camp['health']} \n\t🥫Remaining food:{camp['food']}")
     elif camp['food'] < camp['survivors']:
         rations = camp['food'] / camp['survivors']
-        print(f"Only {camp['food']} food for {camp['survivors']} survivors  ")
-        print(f"Each person gets at least {rations:.2f} food.")
+        print(f"⚠️ Not enough food! Each survivor gets only {rations:.2f} units.")
         # re initialize to 0
         camp['food'] = 0
         camp['health'] = max(0, camp['health'] - 15)
-        print(f"Camp was underfed. Health dropped to {camp['health']}.")
+        print(f"💔Camp was underfed. Health dropped to {camp['health']}.")
 
 # main menu for the options
 def menu():
-    print(f"\t1. Gather food \n\t2. Rest")
+    print(f"\t1️⃣ Gather food \n\t2️⃣ Rest")
 
 def gameover():
     if not won and (camp['health'] <= 0 or camp['food'] <= 0 or camp['survivors'] <= 0):
-        print("Damn, Your camp did not survive! Game over.")
+        print("\n💀 GAME OVER 💀")
+        print("Damn, Your camp did not survive!")
     
 # main loop - as long the health key value is more than 0
 while camp['health'] > 0 and camp['food'] > 0 and camp['survivors'] > 0:
-    print("\n--------------- Camp Survival - Day", camp['day'], "---------------")
+    print("\n📆--------------- Camp Survival - Day", camp['day'], "---------------")
                             
     showStatus()
-    print("This is what you can do:")
+    print("What would you like to do today?: ")
     menu()
-    user_choice = input("Your choice? ")
+    user_choice = input("🎮Your choice (1 or 2)? ")
 
     if user_choice == "1":
         gatherfood()
     elif user_choice == "2":
         rest()
+    else:
+        print("❌ Invalid option. Try again.")
+        continue
 
     if camp['health'] < 40:
         camp['survivors'] = max(0, camp['survivors'] - 2)
-        print("Health is less than 40. You just killed 2 survivors")
+        print("⚠️ Health is low. You just killed 2 survivors")
     
 
     if camp['survivors'] <= 0:
-        print("You just killed everyone. Damn!")
+        print("💥 You just killed everyone. 😢")
+        print("Wanauliza, Je, ulikuwepo?")
         break
     
     feedsurvivors()
@@ -92,7 +96,8 @@ while camp['health'] > 0 and camp['food'] > 0 and camp['survivors'] > 0:
 
     if camp['day'] >= 15:
         won = True
-        print("You just survived more than 15 days. You win the game!!")
+        print("\n🎉🎉 YOU WIN! 🎉🎉")
+        print("🏕️ You survived more than 15 days in the wild!")
         break
     
 gameover()
