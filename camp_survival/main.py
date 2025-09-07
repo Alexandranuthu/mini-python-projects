7a448a4fc70de88dc3eeaa7a0ce8ae52827e9208
Alexandranuthu <alexandranuthuu@gmail.com>
2025-09-07 15:20:49 +0300

    Add function to allocate survivors to gather food

--- camp_survival/main.py
+++ camp_survival/main.py
@@ -1,5 +1,8 @@
+import random
+
 print("\n--------------- Welcome to Camp Survival ---------------")
 
+# dic to store the initial resources
 camp = {
     'day': 1,
     'food': 20,
@@ -10,3 +13,14 @@ camp = {
 def showStatus():
     print(f"\nYou are on day {camp['day']}")
     print(f"So far you are left with \n\t Food: {camp['food']} \n\t Health: {camp['health']} \n\t Survivors: {camp['survivors']}")
+
+
+def gatherfood():
+    print("You have decided to gather food. You need to choose the number of survivors you want to go gather food")
+    no_survivors = int(input("How many? "))
+    # randomly choosing the number of food brought back
+    food_found = random.choice(range(0,15))
+    camp['food'] = camp['food'] + food_found
+    
+    print(f"The {no_survivors} survivors brought back {food_found} food.\nYou now have {camp['food']}")
+
