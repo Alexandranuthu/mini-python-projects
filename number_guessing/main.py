1b2b513dc08a1d0d39be8cfb3b83a83dc7e4cb30
Alexandranuthu <alexandranuthuu@gmail.com>
2025-11-13 20:14:25 +0300

    while loop to to check user input

--- number_guessing/main.py
+++ number_guessing/main.py
@@ -4,12 +4,44 @@ from random import randint
 print("Number Guessing Game")
 print("\tPlease enter a the maximum value and minimum value")
 
-upper_number = int(input("Please enter maximum value: "))
-lower_number = int(input("Please enter minimum value: "))
+upper = int(input("Please enter maximum value: "))
+lower = int(input("Please enter minimum value: "))
 
 #generate a random number from selected range
-selected = randint(upper_number, lower_number)
+selected = randint(upper, lower)
 
 print(selected)
+
+#maximum allowed guesses
+print(f"\n You have 7 chances to guess the number between {upper} and {lower}")
+
+chances = 7
+g_counter = 0
+
+while g_counter < chances:
+    g_counter += 1
+    guess  = int(input("\tPlease enter your guess: "))
+
+
+    if guess == selected:
+        print(f"That is correct!You only guessed in {g_counter} tries. Nice :)")
+        break
+    elif g_counter >=  chances and guess != selected:
+        print(f"You unfortunately did not guess the number.It was: {selected}")
+        print("Better luck next time!")
+    
+    elif guess > selected:
+        print("That is not correct, Your guess is too high!")
+        print("Try a lower number.")
+    elif guess < selected:
+        print("That is not correct, Your guess is too low.")
+        print("Try a higher number.")
+
+   
+
+
+
+
+
 
 
