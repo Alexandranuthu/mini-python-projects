eefa792aa0d8ff24ec049ba0fe2ffcb5c692b6a8
Alexandranuthu <alexandranuthuu@gmail.com>
2025-11-13 20:23:46 +0300

    commented on the code

--- number_guessing/main.py
+++ number_guessing/main.py
@@ -12,23 +12,28 @@ selected = randint(upper, lower)
 
 print(selected)
 
-#maximum allowed guesses
+#maximum allowed chances to guess and tracking the amount of guesses
 print(f"\n You have 7 chances to guess the number between {upper} and {lower}")
 
 chances = 7
 g_counter = 0
 
+#if number of guesses are less than the chances 
 while g_counter < chances:
+    #increment the counter at each guess
     g_counter += 1
     guess  = int(input("\tPlease enter your guess: "))
 
-
+    #once the guess is correct stop the game
     if guess == selected:
         print(f"That is correct!You only guessed in {g_counter} tries. Nice :)")
         break
+
+    # if the number of guesses are more than the chances and the user still hasn't guessed correctly end it
     elif g_counter >=  chances and guess != selected:
         print(f"You unfortunately did not guess the number.It was: {selected}")
         print("Better luck next time!")
+        print("Play again? ")
     
     elif guess > selected:
         print("That is not correct, Your guess is too high!")
@@ -36,6 +41,8 @@ while g_counter < chances:
     elif guess < selected:
         print("That is not correct, Your guess is too low.")
         print("Try a higher number.")
+
+
 
    
 
